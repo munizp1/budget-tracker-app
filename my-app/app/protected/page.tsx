a2f33df9d37b195d02cf368e5b64d240538bf82d
Chris A Soria <chrissoriafw@gmail.com>
2024-03-18 14:57:49 -0400

    Added model to protected page/ fade out double click for model to show dashboard

--- my-app/app/protected/page.tsx
+++ my-app/app/protected/page.tsx
@@ -11,6 +11,7 @@ import { Canvas } from "@react-three/fiber";
 import {Suspense} from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import {OrbitControls} from '@react-three/drei'
+import Homepage from '../../components/homepage/homepage'
 
 export default async function ProtectedPage() {
   const supabase = createClient();
@@ -48,39 +49,10 @@ export default async function ProtectedPage() {
           user, Hey, {userProfile ? userProfile[0].first_name : 'User'}!
      
         </div>
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            
-            
-          </div>
-        </nav>
+        
       </div>
-
-      
-
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-      <li><a href="/addpayment4"> ADD PAYMENT BUTTON</a></li>
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps!!!</h2>
-          <FetchDataSteps />
-        </main>
-      </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
     </div>
+    <Homepage/>
     </Layout>
   ):(
     <div>
