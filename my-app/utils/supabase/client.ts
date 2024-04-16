db73e20fa238f2aa548d6a688bc5cffe0805bc65
Paulo Muniz <munizp1@southernct.edu>
2024-04-16 19:47:08 -0400

    Admin page with some features

--- my-app/utils/supabase/client.ts
+++ my-app/utils/supabase/client.ts
@@ -1,10 +1,11 @@
-import { createBrowserClient } from "@supabase/ssr";
+// utils/supabase/client.ts
+import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Supabase URL and Anon Key must be defined');
+}
 
-
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
